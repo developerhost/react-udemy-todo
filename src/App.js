c206ef606849382c674cf3919e960ffd8f61f30b
dirtyman <50170789+dirtyman69@users.noreply.github.com>
2022-05-02 17:07:10 +0900

    useEffect useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const [num, setNum] = useState(0);
+  const [displayFlag, setDisplayFlag] = useState(false);
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+  const onClickSwitchDisplayFlag = () => {
+    setDisplayFlag(!displayFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        displayFlag || setDisplayFlag(true);
+      } else {
+        displayFlag || setDisplayFlag(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>hello</h1>
       <ColorfulMessage color="pink" message="change" />
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>countUp</button>
+      <button onClick={onClickSwitchDisplayFlag}>on/off</button>
+      <p>{num}</p>
+      {displayFlag && <p>display</p>}
     </>
   );
 };
